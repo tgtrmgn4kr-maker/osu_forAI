dcebdddcbc079d7c832fece86c0c5429e80261a1
OwenSong <tgtrmgn4kr@privaterelay.appleid.com>
2026-07-22 22:25:41 +0800

    Save the training data in a more stable place

--- osu.Game.Rulesets.Osu/AI/Learn/HumanPlayRecorder.cs
+++ osu.Game.Rulesets.Osu/AI/Learn/HumanPlayRecorder.cs
@@ -17,7 +17,7 @@ namespace osu.Game.Rulesets.Osu.AI.Learn
         private OsuReplayRecorder osuReplayRecorder;
         private ObjectTracker objectTracker;
         private RewardTracker rewardTracker;
-        private string filePath = @"D:\Programming\Data\OsuTrainingData.bin";
+        private string filePath = @"C:\Users\user\Data\OsuTrainingData.bin";
 
         public HumanPlayRecorder(ObjectTracker objectTracker, RewardTracker rewardTracker, OsuReplayRecorder osuReplayRecorder)
         {
